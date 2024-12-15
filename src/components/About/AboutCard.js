579eb91930be2d9a58231115d20dec27bbc81ae9
𝗗𝗔𝗥𝗥𝗘𝗟𝗟 𝗠𝗨𝗖𝗛𝗘𝗥𝗜 ⚡ <darrellmucheri@yahoo.com>
2024-12-15 16:03:11 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Darrell M</span>
+            from <span className="purple"> Harare, Zimbabwe.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently not employed .
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed my O level In 2023 while doing sciences
+            
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Mr Frank OFC</footer>
         </blockquote>
       </Card.Body>
     </Card>
